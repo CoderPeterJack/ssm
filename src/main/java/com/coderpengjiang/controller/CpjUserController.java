f9404a5a3b6958154d717f0cc244504aaa76e74e
CoderJiang <1179191373@qq.com>
2019-10-22 22:52:54 +0800

    ssm

---
+++ src/main/java/com/coderpengjiang/controller/CpjUserController.java
@@ -0,0 +1,10 @@
+package com.coderpengjiang.controller;
+
+/**
+ * @program: ssm
+ * @description:
+ * @author: CoderPengJiang
+ * @create: 2019-10-22 19:12
+ **/
+public class CpjUserController {
+}
---
+++ src/main/java/com/coderpengjiang/service/impl/CpjUserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.coderpengjiang.service.impl;
+
+/**
+ * @program: ssm
+ * @description: 继承CpjUserDao接口
+ * @author: CoderPengJiang
+ * @create: 2019-10-22 19:03
+ **/
+public class CpjUserServiceImpl {
+}
